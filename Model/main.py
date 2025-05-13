7771dd24ee8fc6128d3e192a34a02a1d97d2ce69
NADAV MENAHEM <NADAVMEN@rafael.co.il>
2025-05-13 13:36:58 +0300

    update

--- Model/main.py
+++ Model/main.py
@@ -16,7 +16,6 @@ from model_utils import train_model, evaluate_model, save_model, load_model
 
 #==================CONFIG==================
 
-DATA_PLOTTING = False  # Set to True to plot the data
 RESULT_PLOTTING = True  # Set to True to plot the results
 DEBUGGING = False  # Set to True to enable debugging mode
 
@@ -31,7 +30,7 @@ def main():
     config = get_config(args.config)
 
     dataset_path = os.path.join(config.dataset.path, config.dataset.network_name)
-    metadata = get_meta_data(dataset_path)
+    # metadata = get_meta_data(dataset_path)
     G = get_graph(dataset_path)
 
     train_loader, test_loader, cal_loader, X_shape = get_data_loaders(dataset_path, config.batch_size, config.dataset.test_size, config.dataset.cal_size)
@@ -78,26 +77,6 @@ def main():
         #     print(f"Sample {i}: sum = {np.sum(y)}, y = {y}")
         print("DEBUGGING: number of nodes:", G.number_of_nodes())
         print("DEBUGGING: number of edges:", G.number_of_edges())
-
-    if DATA_PLOTTING:
-        X, Y = load_dataset(dataset_path)
-        case_number = 3
-        bus_index = np.argmax(Y[case_number])  # Find the index of the bus with a fault
-        print(f"Bus index: {bus_index}")
-
-        sampling_rate = metadata["sampling_rate"]
-        total_time = metadata["total_time"]
-        outage_time = metadata["outage_time"]
-
-        case_data = X[case_number]  # shape: (T, 1, N) or (T, N)
-
-        if case_data.ndim == 3:
-            case_data = case_data.squeeze(1)  # remove feature dimension if needed
-
-        plot_all_buses(X[case_number], total_time=total_time, sampling_rate=sampling_rate, outage_time=outage_time)
-
-        # plot_data(X[case_number], bus_index=3, sampling_rate=sampling_rate, total_time=total_time,
-        #            outage_time=outage_time, fixed_ylim=y_lim)
 
 
 if __name__ == "__main__":
