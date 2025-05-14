487e5bd670cc3ec5cb50f3b8ddb379fd69a6367d
nadavmenahem <118202630+nadavmenahem@users.noreply.github.com>
2025-05-14 15:29:55 +0300

    update

--- Model/run.sh
+++ Model/run.sh
@@ -6,3 +6,6 @@ python Model/main.py --config Model/config.yaml
 
 # to run plot_data:
 python Model/plot_data.py --config Model/config.yaml
+
+# to run generate_noisy_data:
+python Simulations/PandaPower/generate_noisy_data.py --config Simulations/PandaPower/data_config.yaml
